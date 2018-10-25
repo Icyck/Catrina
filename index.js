372baddde8d491b8dfb7f2144c5e3c77737cb6ff
Icyck <44445398+Icyck@users.noreply.github.com>
2018-10-25 21:12:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -21,21 +21,21 @@ robot.on("ready", () => {
     let cmd = messageArray[0]
     let args = messageArray.slice(1);
 
-    robot.on("guildMemberAdd", async member => {
-      console.log(`${member.id} joined the server.`);
-  
-      let welcomechannel = member.guild.channels.find(`name`, "welcome_leaves");
-      
-      welcomechannel.send(`Hello ${member} welcome to Night Support! Read "<#495869652522500096>" and "<#499934754523840512>". Write in "<#495869103534243840>" ***-verify***. Good bye!`);
+   
   
   
-    });
+   
    
   });
   
 
 
+   robot.on("guildMemberAdd", async member => {
+      console.log(`${member.id} joined the server.`);
   
+      let welcomechannel = member.guild.channels.find(`name`, "welcome_leaves");
+      
+      welcomechannel.send(`Hello ${member} welcome to Night Support! Read "<#495869652522500096>" and "<#499934754523840512>". Write in "<#495869103534243840>" ***-verify***. Good bye!`);
 
  
 
