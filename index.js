d00e9c7ff580b51ebea6eae4aac09149479e44a9
Icyck <44445398+Icyck@users.noreply.github.com>
2018-10-25 21:04:06 +0300

    index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 laconfig = require("./laconfig.json");
-const token = proces.env.token;
 const Discord = require("discord.js");
 const robot = new Discord.Client();
 
@@ -21,23 +20,38 @@ robot.on("ready", () => {
     let messageArray = message.contest.split(" ");
     let cmd = messageArray[0]
     let args = messageArray.slice(1);
+
+    robot.on("guildMemberAdd", async member => {
+      console.log(`${member.id} joined the server.`);
+  
+      let welcomechannel = member.guild.channels.find(`name`, "welcome_leaves");
       
-       robot.on("guildMemberAdd", async member => {
-    console.log(`${member.id} joined the server.`);
-
-    let welcomechannel = member.guild.channels.find(`name`, "welcome_leaves");
-    
-    welcomechannel.send(`Hello ${member} welcome to Night Support! Read "<#495869652522500096>" and "<#499934754523840512>". Write in "<#495869103534243840>" ***-verify***. Good bye!`);
-
-
-  });
-
- if(cmd === `${prefix}ping`){
-     message.reply("Pong")
+      welcomechannel.send(`Hello ${member} welcome to Night Support! Read "<#495869652522500096>" and "<#499934754523840512>". Write in "<#495869103534243840>" ***-verify***. Good bye!`);
+  
+  
+    });
+    if(cmd === `${prefix}verify`){
+      if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Sorry pal, you can't do that.");
+      let pMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+      if(!pMember) return message.reply("Couldn't find that user, yo.");
+     
+      let gRole = message.guild.roles.find(`name`, 'verifed')
+      
+  
+      if(pMember.roles.has(gRole.id))(" Have already role!");
+      await(pMember.addRole(gRole.id));
+  
+      try{
+      await    pMember.send(`You verify account`);
+      }catch(e){
+      message.channel.send(`***You verify account***`);
+      }
     }
   });
   
 
+
+  
 
  
 
